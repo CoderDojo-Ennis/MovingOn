f1b5d81aa5184de73009e8704282dfa51319a938
Dylan Chan <dillchan10@gmail.com>
2017-03-31 21:57:12 +0100

    Added new Instructions
    Added Collision Detection, Destroy and Movement.
    
    Added Sample Unity Sample Project.

---
+++ UnitySample/Sample_Proj/Assets/Destroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Enemy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
