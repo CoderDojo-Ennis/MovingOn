307c57c7e5873a31c3e0cf2d8a0e642b01ef6ef5
Dylan Chan <dillchan10@gmail.com>
2017-04-06 22:16:28 +0100

    Added content to C# and changed 3D to 2D.
    Added more content to C# fundamentals.
    
    Changed Unity C# code from 3D to 2D.
    
    Added background colour to html pages.
    
    Minor changes to textbox area.

---
+++ UnitySample/Sample2D_Proj/Assets/Destroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	// Update is called once per frame
+	void OnCollisionEnter2D (Collision2D col) {
+        
+        if(col.gameObject.name == "Enemy")
+        {
+            Destroy(col.gameObject);   
+        }
+	
+	}
+}
